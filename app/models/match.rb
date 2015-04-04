bedefe00ff847f4023760c29df6f5ce8a21a8f69
Sam <samnap+github@gmail.com>
2015-04-04 15:58:47 -0400

    Improve date validation Use number field instead of text

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,7 +3,8 @@ class Match < ActiveRecord::Base
             presence: true
   validates :visitor_points, :home_points,
             numericality: {only_integer: true, greater_than_or_equal_to: 0}
-  
+  validates_date :date
+
   def score
     self.visitor_points.to_s + " - " + self.home_points.to_s
   end
