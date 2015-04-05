9063e9cb4569e0eda56b0961823760588853116d
Sam <samnap+github@gmail.com>
2015-04-05 19:08:08 -0400

    Add a few comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,28 @@
 
 require 'mechanize'
 
+# Location to scrape data from
 location = "http://www.livescore.com/soccer/usa/usl-pro/results/30-days/"
 
+# Create Mechanize object to fetch url
 puts "Fetching data from #{location}..."
 agent = Mechanize.new
 r = agent.get(location)
+
+# Create Nokogiri parser
 parser = r.parser
 puts "Parsing elements..."
 
+# Testing - parse a local file instead of fetching url
 #parser = Nokogiri::HTML(open("/Users/sam/work/scraper/mechanize/LiveScore Soccer - USL Pro Last 7 Days Results, USA.html"))
 
-dt_s = nil
+dt_s = nil    # date string
 count = 0
 
 # Look at each div plucking ones needed
+# Have to go through them all as date is outside of match element, so
+# once a date is found, subsequent matches were on that date until next
+# date found.
 parser.xpath('//div').each do |div|
 
   # Get the date
