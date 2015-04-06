feb0cd3dc4348ae6bcbd9c124c1395de17962e8b
Sam <samnap+github@gmail.com>
2015-04-05 22:33:57 -0400

    Setup postgres for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150401235034) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "matches", force: :cascade do |t|
     t.string   "visitor"
     t.string   "home"
